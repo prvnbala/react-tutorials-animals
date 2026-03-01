9c42df081240eececcf3927e14c014d098060cc3
praveen <praveen.balasundram@gmail.com>
2026-03-01 13:09:06 +0530

    make heart size increase with clicks

--- src/AnimalShow.jsx
+++ src/AnimalShow.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import BirdImage from "./assets/bird.svg"
 import CatImage from "./assets/cat.svg"
 import CowImage from "./assets/cow.svg"
@@ -16,9 +17,15 @@ const animals = {
 }
 
 function AnimalShow(props) {
-    return <div>
+    const [clicks, setClicks] = useState(0);
+
+    function handleClick() {
+        setClicks(clicks + 1);
+    }
+
+    return <div onClick={handleClick}>
         <img src={animals[props.type]} alt="animal" />
-        <img src={HeartImage} alt="heart" />
+        <img src={HeartImage} alt="heart" style={{"width": 10 + 10*clicks + "px"}}/>
     </div>;
 }
 
