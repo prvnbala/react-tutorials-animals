80f9ab660fd558af5d4534183d753f2229cb11dc
praveen <praveen.balasundram@gmail.com>
2026-03-01 10:05:56 +0530

    import other images into AnimalShow.jsx

--- src/AnimalShow.jsx
+++ src/AnimalShow.jsx
@@ -1,4 +1,10 @@
 import BirdImage from "./assets/bird.svg"
+import CatImage from "./assets/cat.svg"
+import CowImage from "./assets/cow.svg"
+import DogImage from "./assets/dog.svg"
+import GatorImage from "./assets/gator.svg"
+import HeartImage from "./assets/heart.svg"
+import HorseImage from "./assets/horse.svg"
 
 function AnimalShow() {
     return <img src={BirdImage} alt="" />;
